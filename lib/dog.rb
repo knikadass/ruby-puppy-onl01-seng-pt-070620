6041e9050ea607f9f3b83e899897fcb405f24669
Knika Dass <knikadass@gmail.com>
2020-07-28 01:28:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Add your code here
+class Dog 
+  attr_accessor :name 
+  
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+    self.save
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+end
